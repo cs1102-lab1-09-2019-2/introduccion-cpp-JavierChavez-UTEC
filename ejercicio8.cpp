c653b5421228fe6eac07d7ab86c0f130b2dcd18b
JavierChavez-UTEC <54275602+JavierChavez-UTEC@users.noreply.github.com>
2019-08-25 15:49:21 -0500

    Ejercicio 8 arreglado.

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -3,10 +3,8 @@ using std::cout;
 
 int main() {
     auto i = 0;
-    // Mostrar los numeros del 0 al 4
-    while (i < ) {
+    while (i < 4) {
         cout<< i <<"\n";
-
+        i++;
     }
 }
-
