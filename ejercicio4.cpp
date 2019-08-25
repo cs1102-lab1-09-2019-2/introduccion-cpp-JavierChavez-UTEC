6a5f263967de9fcb421e05c7192f788cbd71e815
JavierChavez-UTEC <54275602+JavierChavez-UTEC@users.noreply.github.com>
2019-08-25 15:42:44 -0500

    Ejercicio 4 arreglado

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -1,17 +1,19 @@
+#include <iostream>
+
+using namespace std;
 
 int main() {
+    int i;
+    int c;
     i = 1;
 
-    // Post-incremento asigna i a c y luego incrementa i
     c = i++;
     cout<<"Post-incremento ejemplo:\n";
     cout<<"El valor de c es: "<<c<<"\n";
     cout<<"El valor de i es: "<<i<<"\n";
     cout<<"\n";
-    // resetear i a 1
     i = 1;
 
-    // Pre-incremento incrementa i, luego asigna a c
     c = ++i;
 
     cout<<"Pre-incremento ejemplo:\n";
@@ -19,10 +21,8 @@ int main() {
     cout<<"El valor de i es: "<<i<<"\n";
     cout<<"\n";
 
-    // Decremento
     i--;
     cout<<"Decremento ejemplo: \n";
     cout<<"El valor de i es: "<<i<<"\n";
 
 }
-
