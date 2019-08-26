326858cca2d1ffc4fd6644021eb19dd77a9e726d
JavierChavez-UTEC <54275602+JavierChavez-UTEC@users.noreply.github.com>
2019-08-25 19:27:39 -0500

    Ejercicio 10 arreglado.

--- ejercicio10.cpp
+++ ejercicio10.cpp
@@ -1,4 +1,7 @@
 #include <string>
+#include <iostream>
+#include <vector>
+using std::cout;
 using std::string;
 
 int main() {
@@ -12,10 +15,10 @@ int main() {
     cout<<b[0]<<"\n";
     cout<<b[4]<<"\n";
 
-    int numeros{4, 8, 9, 5, 6, 7};
+    std::vector<int> numeros{4, 8, 9, 5, 6, 7};
     cout<<numeros[0]<<"\n";
 
     float numero{5.16};
-    cout<<numero<<"\n"
+    cout<<numero<<"\n";
 
 }
